90d79d809629f39bd45662df8130b3828f3389a8
PalharesPe <eraldo.ferreira@alunos.ufersa.edu.br>
2021-04-02 19:20:35 -0300

    Iniciando CRUD da unidade 1

---
+++ cadpessoas_eraldo/src/main/java/br/com/eraldo/cadpessoas/CadpessoasEraldoApplication.java
@@ -0,0 +1,13 @@
+package br.com.eraldo.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasEraldoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasEraldoApplication.class, args);
+	}
+
+}
---
+++ cadpessoas_eraldo/src/test/java/br/com/eraldo/cadpessoas/CadpessoasEraldoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eraldo.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasEraldoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
