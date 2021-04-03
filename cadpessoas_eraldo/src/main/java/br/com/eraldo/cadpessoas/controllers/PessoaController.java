80c1c417cff534806a219b2e558758b97227dafb
PalharesPe <eraldo.ferreira@alunos.ufersa.edu.br>
2021-04-02 21:39:09 -0300

    Adicionando Index

--- cadpessoas_eraldo/src/main/java/br/com/eraldo/cadpessoas/controllers/PessoaController.java
+++ cadpessoas_eraldo/src/main/java/br/com/eraldo/cadpessoas/controllers/PessoaController.java
@@ -1,8 +1,23 @@
 package br.com.eraldo.cadpessoas.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.eraldo.cadpessoas.repositorys.PessoaRepository;
 
 @Controller
+@RequestMapping("/")
 public class PessoaController {
+	
+	@Autowired
+	PessoaRepository pessoaRepo;
+	
+	@GetMapping
+	public String index() {
+		return "index.html";
+	}
 
 }
--- cadpessoas_eraldo/src/main/java/br/com/eraldo/cadpessoas/repositorys/PessoaRepository.java
+++ cadpessoas_eraldo/src/main/java/br/com/eraldo/cadpessoas/repositorys/PessoaRepository.java
@@ -1,5 +1,11 @@
 package br.com.eraldo.cadpessoas.repositorys;
 
-public class PessoaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.eraldo.cadpessoas.model.Pessoa;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
 
 }
