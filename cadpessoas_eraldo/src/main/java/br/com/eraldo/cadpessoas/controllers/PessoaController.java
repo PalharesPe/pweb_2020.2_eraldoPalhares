18a999f88ed82a29b1e71345aabde1d44e0dddaf
PalharesPe <eraldo.ferreira@alunos.ufersa.edu.br>
2021-04-03 22:08:26 -0300

    Alterações no menu e Style nas paginas

--- cadpessoas_eraldo/src/main/java/br/com/eraldo/cadpessoas/controllers/PessoaController.java
+++ cadpessoas_eraldo/src/main/java/br/com/eraldo/cadpessoas/controllers/PessoaController.java
@@ -1,23 +1,34 @@
 package br.com.eraldo.cadpessoas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.eraldo.cadpessoas.model.Pessoa;
 import br.com.eraldo.cadpessoas.repositorys.PessoaRepository;
 
 @Controller
 @RequestMapping("/")
 public class PessoaController {
-	
+
 	@Autowired
 	PessoaRepository pessoaRepo;
-	
+
 	@GetMapping
 	public String index() {
 		return "index.html";
 	}
 
+	@GetMapping("/listarPessoas")
+	public ModelAndView listarPessoas() {
+		List<Pessoa> lista = pessoaRepo.findAll();
+		ModelAndView mav = new ModelAndView("listarPessoas");
+		mav.addObject("pessoas", lista);
+		return mav;
+	}
+	
 }
