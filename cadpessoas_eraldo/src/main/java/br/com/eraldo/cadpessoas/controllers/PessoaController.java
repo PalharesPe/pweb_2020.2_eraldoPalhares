de80660ac39dabb16ff3f15cf507a2b9864ce779
PalharesPe <eraldo.ferreira@alunos.ufersa.edu.br>
2021-04-08 22:14:05 -0300

    alterações nas paginas e no controller

--- cadpessoas_eraldo/src/main/java/br/com/eraldo/cadpessoas/controllers/PessoaController.java
+++ cadpessoas_eraldo/src/main/java/br/com/eraldo/cadpessoas/controllers/PessoaController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -30,5 +32,39 @@ public class PessoaController {
 		mav.addObject("pessoas", lista);
 		return mav;
 	}
-	
+
+	@GetMapping("/adicionarPessoa")
+	public ModelAndView formAdicionarPessoa() {
+		ModelAndView modelAndView = new ModelAndView("adicionarPessoa");
+		modelAndView.addObject(new Pessoa());
+		return modelAndView;
+	}
+
+	@PostMapping("/adicionarPessoa")
+	public String adicionarPessoa(Pessoa p) {
+		this.pessoaRepo.save(p);
+		return "redirect:/listarPessoas";
+
+	}
+
+	@GetMapping("/editar/{id}")
+	public ModelAndView formEditarPessoa(@PathVariable("id") long id) {
+		Pessoa pessoa = pessoaRepo.findById(id).orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id));
+		ModelAndView modelAndView = new ModelAndView("editarPessoa");
+		modelAndView.addObject(pessoa);
+		return modelAndView;
+	}
+	@PostMapping("/editar/{id}")
+	public ModelAndView editarPessoa(@PathVariable("id") long id, Pessoa pessoa) {
+		this.pessoaRepo.save(pessoa);
+		return new ModelAndView("redirect:/listarPessoas");
+	}
+
+	@GetMapping("/remover/{id}")
+	public ModelAndView removerPessoa(@PathVariable("id") long id) {
+		Pessoa aRemover = pessoaRepo.findById(id).orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id));
+		pessoaRepo.delete(aRemover);
+		return new ModelAndView("redirect:/listarPessoas");
+	}
+
 }
